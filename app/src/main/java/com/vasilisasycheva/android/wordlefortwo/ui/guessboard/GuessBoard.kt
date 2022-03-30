5a31a45578041fbca7b781e7f1d2187a45237822
arbaktos <71153918+arbaktos@users.noreply.github.com>
2022-03-30 19:52:24 +0300

    guessboard space is ready

--- app/src/main/java/com/vasilisasycheva/android/wordlefortwo/ui/guessboard/GuessBoard.kt
+++ app/src/main/java/com/vasilisasycheva/android/wordlefortwo/ui/guessboard/GuessBoard.kt
@@ -3,6 +3,8 @@ package com.vasilisasycheva.android.wordlefortwo.ui.guessboard
 import android.content.Context
 import android.text.InputFilter
 import android.util.AttributeSet
+import android.view.Gravity
+import android.view.View.MeasureSpec.getSize
 import android.view.ViewGroup
 import androidx.core.view.children
 import com.vasilisasycheva.android.wordlefortwo.R
@@ -16,47 +18,44 @@ class GuessBoard@JvmOverloads constructor(
 ) : ViewGroup(ctx, attributeSet, defStyle) {
 
     private var squareWidth = 0
+    private var squareHeight = 0
     private val padding = 10
+    private var gbWidth = 0
+    private var gbHeight = 0
 
     init {
         repeat(6) {
-            addView(RowOfSquares(ctx).apply {
-                pad = padding
-            })
+            addView(RowOfSquares(ctx))
         }
     }
 
     override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
-        squareWidth = (width - padding * 6) / 7
-        children.forEach { it as RowOfSquares
-            it.sqWidth = squareWidth
-        }
         var top = 0
-        var bottom = squareWidth
+        var bottom = squareHeight
         children.forEach { child ->
             child.layout(l, top, r, bottom)
-            top += squareWidth
+            top += squareHeight
             bottom += top + padding
         }
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        gbHeight = getSize(heightMeasureSpec)
+        gbWidth = getSize(widthMeasureSpec)
+
         var usedHeight = 0
         children.forEach { child ->
-            child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED))
-            usedHeight += child.measuredHeight
+            child.measure(widthMeasureSpec, heightMeasureSpec)
+            usedHeight += child.measuredHeight + padding
         }
         setMeasuredDimension(widthMeasureSpec, usedHeight + padding)
     }
 
-    class RowOfSquares @JvmOverloads constructor(
+    inner class RowOfSquares @JvmOverloads constructor(
         val ctx: Context,
         attributeSet: AttributeSet? = null,
         defStyle: Int = 0
     ) : ViewGroup(ctx, attributeSet, defStyle) {
-
-        var sqWidth = 0
-        var pad = 0
 
         init {
             repeat(5) { num ->
@@ -66,25 +65,22 @@ class GuessBoard@JvmOverloads constructor(
         }
 
         override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-//            val parentWidth = MeasureSpec.getSize(widthMeasureSpec);
-//            val parentHeight = MeasureSpec.getSize(heightMeasureSpec)
             val child = children.first()
-            child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED))
-            val height = child.measuredHeight * 3 - pad
+            child.measure(widthMeasureSpec, heightMeasureSpec)
+            val height = child.measuredHeight
             setMeasuredDimension(widthMeasureSpec, height)
-//            setMeasuredDimension(parentWidth, parentHeight/6)
         }
 
         override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
-            var left = sqWidth + pad
+            var left = (gbWidth - (squareWidth + padding) * 5) / 2
             children.forEach { it as Square
-                it.layout(left, pad, left + sqWidth, sqWidth)
-                left += sqWidth + pad
+                it.layout(left, padding, left + squareWidth, squareHeight)
+                left += squareWidth + padding
             }
         }
     }
 
-    class Square(private val ctx: Context): androidx.appcompat.widget.AppCompatEditText(ctx, null, 0) {
+    inner class Square(private val ctx: Context): androidx.appcompat.widget.AppCompatEditText(ctx, null, 0) {
 
         init {
             filters = arrayOf<InputFilter>(InputFilter.LengthFilter(1))
@@ -98,15 +94,20 @@ class GuessBoard@JvmOverloads constructor(
         override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
             val tSize = height * 0.7f
             textSize = ctx.pixelsToSp(tSize)
-            val paddingV = (height * 0.1).toInt()
-            val paddingH = (width * 0.2).toInt()
-            setPadding(paddingH, paddingV, paddingH, paddingV)
+            val paddingV = (height * 0.07).toInt()
+            setPadding(0, paddingV, 0, paddingV)
+            gravity = Gravity.CENTER
             super.onLayout(changed, left, top, right, bottom)
         }
 
         fun setSquareStatus(status: GuessState = GuessState.Default) {
             this.background = ctx.getDrawable(status.etColor)
-            invalidate()
+        }
+
+        override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+            squareHeight = (gbHeight - padding) / 6 //(squareWidth * 1.2).toInt()
+            squareWidth = (squareHeight * 0.8).toInt()
+            setMeasuredDimension(squareWidth, squareHeight)
         }
     }
 
